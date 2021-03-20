30e92ce0871220d6f4ebe2e93c95b986348b8ad0
Webb249 <tom.webb249@gmail.com>
2021-03-20 11:33:22 +0000

    Connect submit button to the web client searcher

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using AbsoluteLabelServicesTechnicalTest.Models;
+using IndexPageUI;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -10,10 +11,27 @@ namespace AbsoluteLabelServicesTechnicalTest.Controllers
 {
     public class HomeController : Controller 
     {
+        static ISearchType searchType = new ItunesSearch();
+        static IWebReader webReader = new WebReader();
+        Index IndexModelData = new Index(searchType, webReader);
+
         public IActionResult Index()
         {
-            return View();
+            return View(IndexModelData);
         }
+
+        // Run when submit button pressed
+        [HttpPost]
+        public async Task<IActionResult> Index(string searchphrase, string entity)
+        {
+            IndexModelData.SearchPhrase = searchphrase;
+            IndexModelData.Entity = entity;
+
+            await IndexModelData.RunGetSearchResults();
+
+            return View(IndexModelData);
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
--- Views/Home/Index.cshtml.cs
+++ Views/Home/Index.cshtml.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 using AbsoluteLabelServicesTechnicalTest.Models;
-
+using System.Threading.Tasks;
 
 namespace IndexPageUI
 {
@@ -12,5 +12,27 @@ namespace IndexPageUI
         public string Entity { get; set; }
         // Store the list of entity types that can be used
         public SearchEntities.iTunes TypeList { get; set; }
+        // Results from the search stored as a string
+        public string SearchReults { get; set; }
+
+        private ISearchType _searchType;
+        private IWebReader _webReader;
+
+        public Index(ISearchType searchType, IWebReader webReader)
+        {
+            this._searchType = searchType;
+            this._webReader = webReader;
+        }
+
+        /// <summary>
+        /// Get the search results
+        /// </summary>
+        public async Task RunGetSearchResults()
+        {
+            _searchType.SetSearchValues(SearchPhrase, Entity);
+            _webReader.SetWebURL(_searchType.GetSearchAddress());
+
+            SearchReults = await Task.Run(() => { return _webReader.GetSearchResults(); });
+        }
     }
 }

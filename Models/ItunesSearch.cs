d2f4b7bc1408b39510f26245f06b7ca4c02075ad
Webb249 <tom.webb249@gmail.com>
2021-03-20 11:31:17 +0000

    Add interface and itunes search class

---
+++ Models/ItunesSearch.cs
@@ -0,0 +1,29 @@
+﻿namespace AbsoluteLabelServicesTechnicalTest.Models
+{
+    /// <summary>
+    /// ISearchType for the itunes api searching
+    /// </summary>
+    public class ItunesSearch : ISearchType
+    {
+        private string SearchAddress = "https://itunes.apple.com/search?";
+        private string _value = "";
+        private string _type = "";
+
+        public void SetSearchValues(string value, string type)
+        {
+            this._value = value;
+            this._type = type;
+        }
+
+        public string GetSearchAddress()
+        {
+            return ConstructAPIAddress();
+        }
+
+        // Construct the address to be used for the itunes API search
+        private string ConstructAPIAddress()
+        {
+            return SearchAddress + "term=" + _value + "&country=GB" + "&entity=" + SearchEntities.iTunesEntityRelationship[_type];
+        }
+    }
+}
---
+++ Models/SearchType.cs
@@ -0,0 +1,15 @@
+﻿
+namespace AbsoluteLabelServicesTechnicalTest.Models
+{
+    /// <summary>
+    /// Interface for the different systems to be used in a search
+    /// </summary>
+    public interface ISearchType
+    {
+        // Set the search values to be used
+        void SetSearchValues(string searchValue, string entity);
+
+        // Return the API address
+        string GetSearchAddress();
+    }
+}

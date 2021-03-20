fdd2a73ef1f06df13854119e5ea81763d9da6e54
Webb249 <tom.webb249@gmail.com>
2021-03-20 15:33:20 +0000

    Add interface for results reading and Json deserialiser

---
+++ Models/IResultsReader.cs
@@ -0,0 +1,11 @@
+﻿
+
+using System.Collections.Generic;
+
+namespace AbsoluteLabelServicesTechnicalTest.Models
+{
+    public interface IResultsReader
+    {
+        Dictionary<string, object> ConvertToResultsInfo(string results);
+    }
+}
---
+++ Models/JSONReader.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AbsoluteLabelServicesTechnicalTest.Models
+{
+    public class JSONReader : IResultsReader
+    {
+        public Dictionary<string, object> ConvertToResultsInfo(string jo)
+        {
+            var values = JsonConvert.DeserializeObject<Dictionary<string, object>>(jo);
+            var values2 = new Dictionary<string, object>();
+            foreach (KeyValuePair<string, object> d in values)
+            {
+                if (d.Value is JObject)
+                {
+                    values2.Add(d.Key, ConvertToResultsInfo(d.Value.ToString()));
+                }
+                else
+                {
+                    values2.Add(d.Key, d.Value);
+                }
+            }
+            return values2;
+        }
+    }
+}
